fe827da195eb51c68b39c9597563e502b2cf1111
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-01 18:24:07 +0300

    remove typos

--- src/components/colortext.jsx
+++ src/components/colortext.jsx
@@ -2,18 +2,19 @@ import React, {useState} from 'react'
 
 const Colortext = () => {
 
-    const[textcolor, setTextcolor] = useState(false);
+    const[textcolor, setTextcolor] = useState("black");
 
     const handleClick = ()=>{
-        setTextcolor(!false)
+        setTextcolor("red")
     }
 
   return (
 
     <div>
         <button onClick={()=> handleClick()}> SHow/Hide Me !</button>
+        <h1 style={{color: textcolor } }>I gotta do what I have to</h1>
         
-        {textcolor?<h1 style={{color: 'green'}}>I gotta do what I have to</h1> : ""}
+        {/* {textcolor?<h1 style={{color: 'green'}}>I gotta do what I have to</h1> : ""} */}
         {/* { textcolor && <h1 style={{color: 'green'}}>I gotta do what I have to</h1> } */}
         
 
