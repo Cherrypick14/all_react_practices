2701a0570df1cbb2de21132a93ee22bde6fa70cc
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-01 17:51:01 +0300

    add textchanger and counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Planets from "./components/planets";
+import Counter from "./components/counter";
+import Textchanger from "./components/textchanger";
 
 function App() {
   return (
     <div className="App">
       <>
       <Planets />
+      <Counter />
+      <Textchanger />
       </>
     </div>
   );
