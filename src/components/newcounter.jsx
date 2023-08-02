a1a563e95830b770c8a118fa4d96ca80723a14c4
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-02 18:52:04 +0300

    finish new counter comp

---
+++ src/components/newcounter.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+const Newcounter = () => {
+
+    const[count, setCount] = useState(0);
+
+
+const handleIncrement = () =>{
+
+        setCount(count+ 1)
+        }
+const handleDecrement = () =>{
+
+        setCount(count-1)
+        }
+const handleSet = ()=>{
+
+        setCount("")
+            
+        }
+
+  return (
+    <React.Fragment>
+      <button onClick={()=>handleIncrement()}>+</button>
+       {count} 
+      <button onClick={()=>handleDecrement()}>-</button>
+
+      <button onClick={()=>handleSet()}> Set to Zero</button>
+
+    </React.Fragment>
+  )
+}
+
+export default Newcounter;
