bd851d2db6423087445f68ae35d17f5215c09a55
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-04 23:36:33 +0300

    add todo list component

---
+++ src/components/todolist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todolist = () => {
+  return (
+    <div className='todo'>
+         <input />
+        <button>Add Task</button>
+      <div className='tasks'></div>
+    </div>
+  )
+}
+
+export default Todolist;
