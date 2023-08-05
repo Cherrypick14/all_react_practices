6762ded094ae73ca757138daca3772d822106535
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-05 23:58:49 +0300

    add value option to input

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,13 +1,18 @@
 import React,{useState} from 'react'
 
 const Todolist = () => {
-  
+
   const[todoList, setTodoList] = useState([]);
   const[newTodo, setNewTodo]= useState("");
 
+  const handleChange = (e) =>{
+    setNewTodo( e.target.value)
+    console.log(e)
+  }
+
   return (
     <div className='todo'>
-         <input />
+         <input onChange={handleChange} value ={newTodo}/>
         <button>Add Task</button>
       <div className='tasks'></div>
     </div>
