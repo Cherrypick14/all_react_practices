6298d00c7cfadb241d771e009c9e78c91cc27e18
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-07 23:45:57 +0300

    modify addtotask object

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -10,22 +10,26 @@ const Todolist = () => {
     
   }
 
- const handleToDo = () =>{
-    const newList = [...todoList, newTodo]
-    setTodoList(newList)
- }
+ const addTask = () =>{
+   const task = {
+    id : todoList.length === 0 ? 1 : todoList[todoList.length - 1 ].id + 1,
+    taskName: newTodo
+  };
+    setTodoList([...todoList, task])
+     
+ };
 
- const deleteTodo = (taskName) => {
-      const newTask =  todoList.filter((task)=>{
-            if( task  ===  taskName ){
-              return false;
-            }else{
-              return true;
-            }
-          }) ;
+ const handleToDo = () =>{
+  const newList = [...todoList, newTodo]
+  setTodoList(newList)
+};
+
+ const deleteTodo = (id) => {
+      const newTask =  todoList.filter((task)=> task.id !== id    
+          ) ;
           setTodoList(newTask);
  }
- 
+  
   return (
     <div className='todo'>
          <input onChange={handleChange} value ={newTodo}/>
@@ -34,9 +38,9 @@ const Todolist = () => {
       <div className='task'>
         {todoList.map((task,key)=>{
           return( 
-            <div key={key}>
-            <h1>{task}</h1>
-            <button onClick={()=> deleteTodo(task)}>X</button>
+            <div key={key.id}>
+            <h1>{task.taskName}</h1>
+            <button onClick={()=> deleteTodo(task.id)}>X</button>
            </div> 
           )
         
