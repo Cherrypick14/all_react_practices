bf0c6bd82a697c1097429fb481d23286107931b5
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-11 23:22:17 +0300

    fix typos

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -6,14 +6,19 @@ const Todolist = () => {
   const[todoList, setTodoList] = useState([]);
   const[newTodo, setNewTodo]= useState("");
 
-  const handleChange = (e) =>{
-    setNewTodo( e.target.value)
+  const handleChange = (event) =>{
+    setNewTodo( event.target.value);
     
   }
 
  const addTask = () =>{
+
+   // determine the next id based on the last item in the todolist
+
+   const nextId = todoList.length === 0 ? 1 : todoList[todoList.length - 1 ].id + 1 ;
+
    const task = {
-    id : todoList.length === 0 ? 1 : todoList[todoList.length - 1 ].id + 1,
+    id : nextId,
     taskName: newTodo,
     completed: false
   };
@@ -28,32 +33,35 @@ const Todolist = () => {
 
     const deleteTodo = (id) => {
       const newTask =  todoList.filter((task)=> task.id !== id    
-          ) ;
+          ) 
           setTodoList(newTask);
  }
 
-  const completeTask = (id) => {
-     setTodoList(
-        todoList.map((task)=>{
-         if(task.id === id){
-            return [...task, completed : true]
-         }else{
-            return task
-         }
-        })
-     )
-  }
+  // const completeTask = (id) => {
+  //    setTodoList(
+  //       todoList.map((task)=>{
+  //        if(task.id === id){
+  //           return [...task, completed : true]
+  //        }else{
+  //           return task
+  //        }
+  //       })
+  //    )
+  // }
   return (
     <div className='todo'>
-         <input onChange={handleChange} value ={newTodo}/>
+         <input onChange={handleChange} />
         <button onClick={handleToDo}>Add Task</button>
 
       <div className='task'>
         {todoList.map((task,key)=>{
           return( 
-            <>
-              <Task taskName={task.taskName} id ={task.id} deleteTodo={deleteTodo} completeTask={completeTask} />
-            </>
+            
+              <Task 
+              taskName={task.taskName} 
+              id ={task.id} 
+              deleteTodo={deleteTodo}  />
+            
           
           )
         
@@ -62,6 +70,6 @@ const Todolist = () => {
         </div>
     </div>
   )
-}
+}                      
 
 export default Todolist;
