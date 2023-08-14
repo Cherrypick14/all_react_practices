ca7cc6e7d201a251ae259ff0d36e4db9acd171e9
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-14 22:14:28 +0300

    add random cat facts components

---
+++ src/components/catfacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Catfacts = () => {
+  return (
+    <div className='cats'>
+        <button>Get me Random Cat Facts</button>
+        <p></p>
+    </div>
+  )
+}
+
+export default Catfacts;
