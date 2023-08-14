3ccedad680fdba24a69f4e017e398ac879ea57bf
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-15 00:28:40 +0300

    fetch Cat api data

--- src/components/catfacts.jsx
+++ src/components/catfacts.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Axios from 'axios'
 
 const Catfacts = () => {
+
+    const [Catfact, setCatfact] = useState("");
+
+    useEffect(()=>{
+
+        fetchFact();
+     
+    },[])
+
+    const fetchFact = () =>{
+
+        Axios.get("https://catfact.ninja/fact").then((res)=>{
+            setCatfact(res.data.fact);
+     
+        })
+    }
   return (
     <div className='cats'>
-        <button>Get me Random Cat Facts</button>
-        <p></p>
+        <button onClick={fetchFact}>Get me Random Cat Facts</button>
+        <p>{Catfact}</p>
     </div>
   )
 }
