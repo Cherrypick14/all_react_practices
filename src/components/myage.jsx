263f80d4fc16bbbf2554a901ea22b54cf1444eb3
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-16 22:49:09 +0300

    create myage component

---
+++ src/components/myage.jsx
@@ -0,0 +1,12 @@
+import React,{useState, useEffect}from 'react'
+import Axios from 'axios'
+
+const Myage = () => {
+  return (
+    <div className='Age'>
+        <button>Predict Age</button>
+    </div>
+  )
+}
+
+export default Myage;
