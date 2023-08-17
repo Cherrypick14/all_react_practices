5d9b18a13ca6f74532e21a3ef1c6f16d8a8c21a1
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-17 15:51:08 +0300

    use agify api to get names

--- src/components/myage.jsx
+++ src/components/myage.jsx
@@ -2,9 +2,23 @@ import React,{useState, useEffect}from 'react'
 import Axios from 'axios'
 
 const Myage = () => {
+
+  const[name, setName] = useState("")
+
+  const fetchData = () =>{
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>{
+      console.log(res.data)
+  })
+  }
+ 
   return (
     <div className='Age'>
-        <button>Predict Age</button>
+      <input placeholder='Exmp .. Cheryl'
+             onChange={(event)=> setName(event.target.value)}/>
+
+        <button onClick={fetchData}>Predict Age</button>
+        <h1>The predicted age is: </h1>
+        
     </div>
   )
 }
