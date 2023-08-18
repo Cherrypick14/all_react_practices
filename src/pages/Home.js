69f2bcae0137a1f199f6d0603d4680393fc40252
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-18 05:28:20 +0300

    add h1 semantic tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export const Home = () =>{
     return (
         <div className="Home">
-            Hi Guys, I'm home already. What's good?
+           <h1>Hi Guys, I'm home already. What's good?</h1> 
         </div>
     )
 };
