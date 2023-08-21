14074e52b828546b6ef6cac5084308bf76a34a3b
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-21 20:29:06 +0300

    pass categ props

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,7 +1,7 @@
-export const Categories = () =>{
+export const Categories = (props) =>{
     return (
         <div className="Categories">
-           <h1>Hey! Gotta choose your fav category</h1>
+           <h1>Hey, I know your fav category of snacks is : {props.category}</h1>
         </div>
     )
 };
