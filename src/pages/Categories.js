34340e373fcbf9aee31bd42484cc6574f2f9a45b
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-22 23:37:48 +0300

    add usecontext hook to pass data

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,7 +1,14 @@
-export const Categories = (props) =>{
+import React,{useContext}from 'react'
+
+import { AppContext } from '../App';
+
+
+export const Categories = () =>{
+    
+    const {category} = useContext(AppContext);
     return (
         <div className="Categories">
-           <h1>Hey, I know your fav category of snacks is : {props.category}</h1>
+           <h1>Hey, I know your fav category of snacks is : {category}</h1>
         </div>
     )
 };
