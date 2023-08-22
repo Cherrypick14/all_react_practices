006fe8fb87edba65f0a38d6cee46cc2308dabcd4
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-22 23:40:45 +0300

    add usecontext hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
-export const Home = (props) =>{
+import React,{useContext} from 'react';
+
+import { AppContext } from '../App';
+
+export const Home = () =>{
+    const{category} = useContext(AppContext);
+    
     return (
         <div className="Home">
-           <h1>Hey, Can I get some of the:{props.category} already</h1> 
+           <h1>Hey, Can I get some of the:{category} already</h1> 
         </div>
     )
 };
