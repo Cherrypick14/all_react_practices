58ee958968c5fa35fbb271761d609b1e4d63dd01
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-22 23:51:29 +0300

    add usecontext practice

--- src/pages/allproducts.js
+++ src/pages/allproducts.js
@@ -1,7 +1,17 @@
+import React,{useState, useContext} from 'react'
+
+import { AppContext } from '../App';
+
+
 export const Contacts = () =>{
+  const[contacts, setContacts] = useState("Myron");
+
+  const{setCategory} = useContext(AppContext);
+
     return (
         <div className="Products">
           <h1>For further info, Contact us here!</h1> 
+          <button onClick={() => setCategory(contacts)}></button>
         </div>
     )
 };
