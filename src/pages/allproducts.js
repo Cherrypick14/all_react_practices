c78b3694cd150c9b1ecb212713027779e2c62afa
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-23 23:05:10 +0300

    commented out unused lines

--- src/pages/allproducts.js
+++ src/pages/allproducts.js
@@ -1,17 +1,17 @@
 import React,{useState, useContext} from 'react'
 
-import { AppContext } from '../App';
+// import { AppContext } from '../App';
 
 
 export const Contacts = () =>{
   const[contacts, setContacts] = useState("Myron");
 
-  const{setCategory} = useContext(AppContext);
+  // const{setCategory} = useContext(AppContext);
 
     return (
         <div className="Products">
           <h1>For further info, Contact us here!</h1> 
-          <button onClick={() => setCategory(contacts)}></button>
+          {/* <button onClick={() => setCategory(contacts)}></button> */}
         </div>
     )
 };
