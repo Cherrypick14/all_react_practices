6c12f6185d0863d8ff243514047880e7a8dbeeeb
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-23 23:04:29 +0300

    correct axios import

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,7 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import  Axios  from "axios";
+
 export const Contacts = () =>{
+
+  const {data} = useQuery(["cat"],()=>{
+
+   return Axios.get("https://catfact.ninja/fact").then((res)=> res.data);
+
+  });
     return (
         <div className="Contacts">
-          <h1>For further info, Contact us here!</h1> 
+          <h1>{data?.fact}</h1> 
         </div>
     )
 };
