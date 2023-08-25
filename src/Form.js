4a83560c1abdc52a1806ced9282313a740b6e3a1
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-25 21:46:00 +0300

    add form

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+export const Form = ()=>{
+    return(
+        <form>
+            <input type="text" placeholder="Full Name ..." />
+            <input type="text" placeholder=""/>
+            <input type="text" placeholder=""/>
+            <input type="password" />
+            <input type="password" />
+            <input type="submit" />
+        </form>
+    )
+}
