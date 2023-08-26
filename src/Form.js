edf42edeb7981592cced039fb8de965f0e0d34f9
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-26 17:51:36 +0300

    add password and confirm password

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,11 @@
 export const Form = ()=>{
     return(
         <form>
-            <input type="text" placeholder="Full Name ..." />
-            <input type="text" placeholder=""/>
-            <input type="text" placeholder=""/>
-            <input type="password" />
-            <input type="password" />
+            <input type="text" placeholder="Enter your Full Name ..." />
+            <input type="text" placeholder=" EnterEmail ..."/>
+            <input type="text" placeholder=" Age ..."/>
+            <input type="password" placeholder="Password..." />
+            <input type="password" placeholder="Confirm password..."/>
             <input type="submit" />
         </form>
     )
