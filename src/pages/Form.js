77728e3144a05301f46e74b00edd53580882962a
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-28 23:30:57 +0300

    add prettier formatter

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,11 +1,25 @@
+import {useForm} from 'react-hook-form';
+
+
 export const Form = ()=>{
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = () =>{
+        console.log("Hi, it is a Monday");
+    }
     return(
-        <form>
-            <input type="text" placeholder="Enter your Full Name ..." />
-            <input type="text" placeholder=" EnterEmail ..."/>
-            <input type="text" placeholder=" Age ..."/>
-            <input type="password" placeholder="Password..." />
-            <input type="password" placeholder="Confirm password..."/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Enter your Full Name..." {...register("fullName")} />
+            <input type="text" placeholder=" Enter your Email ..." {...register("email")}/>
+            <input type="text" placeholder=" Age ..."{...register("age")}/>
+            <input 
+            type="password"
+            placeholder="Password..." 
+            {...register("password")}/>
+            <input 
+            type="password"
+            placeholder="Confirm password..."
+            {...register("confirm password")}/>
             <input type="submit" />
         </form>
     )
