02b75079d3e8a94c0a77544b21b3c4923a89f14d
Cherrypick14 <cherrypicklet@gmail.com>
2023-08-29 00:18:06 +0300

    wrap up on yup schema then added a yup resolver

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,15 +1,20 @@
 import {useForm} from 'react-hook-form';
 import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 export const Form = ()=>{
-    const {register, handleSubmit} = useForm();
-
+  
     const schema = yup.object().shape({
         fullName: yup.string().required(),
-        email:yup.string().required(),
-        age: yup.number(),
-        password:yup.string().required()
-    })
+        email:yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password:yup.string().min(5).max(20).required(),
+        confirmPassword:yup.string().oneOf([yup.ref("password"), null]).required()
+    });
+
+    const {register, handleSubmit} = useForm({
+        resolver: yupResolver(schema)
+    });
 
     const onSubmit = (data) =>{
         console.log(data);
