50f870d6790ceea155404072253acc2b8bde3065
Cherrypick14 <cherrypicklet@gmail.com>
2023-09-04 23:29:25 +0300

    render out data on shawn

--- src/Person.js
+++ src/Person.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 
-export const Person = () =>{
-    <div>
-    </div>
+export const Person = (props) =>{
+
+    return(
+        <div className='person'>
+            <h1>Name: {props.name}</h1>
+            <h2>Email: {props.email}</h2>
+            <h2>Age: {props.age}</h2>
+            <h2>This person{props.isStatus? "married": "single"} at the moment</h2>
+            <>
+            <h1>Shawn Colucci is lcoe to the following friends</h1>
+            {props.allies.map((friends)=>{
+              <div className='friends'>
+                 <h3>{friends}</h3>
+              </div>
+            })}
+            </>
+
+        </div>
+    )
+   
 }
